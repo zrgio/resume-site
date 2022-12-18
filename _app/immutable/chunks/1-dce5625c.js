7636adcb367976a6537c859e3ce507b6d1dd6a9d
Sergio <sergioulloa@protonmail.com>
2022-12-17 22:58:33 -0300

    Updates

---
+++ _app/immutable/chunks/1-dce5625c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-9c614057.js";export{t as component};
---
+++ _app/immutable/chunks/2-44102041.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-9d844d57.js";export{t as component};
