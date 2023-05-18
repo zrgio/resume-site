d1d2c297016dd95d56a03f54ea22179c2a01cd1f
Sergio <sergioulloa@protonmail.com>
2023-05-18 16:49:38 -0400

    Oh well, second try

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@ import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -25,9 +25,11 @@ const config = {
 
 		appDir: 'internal',
 
+		/*
 		paths: {
 			base: dev ? '' : 'https://zrgio.github.io/resume-site'
 		}
+		*/
 	},
 };
 
